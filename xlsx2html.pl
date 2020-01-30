0fadb314798231deeae1cb90af3183f5dc0cd6a7
tsuchim <yuji@eikai.co.jp>
2020-01-30 17:58:45 +0900

    rad test from xlsx

---
+++ xlsx2html.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+use lib 'libs/Spreadsheet-XLSX/lib/';
+use strict;
+use Encode;
+use Spreadsheet::XLSX;
+
+# Open book file
+my $infile = 'data/chapter-0.xlsx';
+my $oBook = Spreadsheet::XLSX->new($infile);
+ 
+# show book info
+print "Filename :", $oBook->{File} , "\n";
+print "Sheet Count :", $oBook->{SheetCount} , "\n";
+print "Author:", $oBook->{Author} , "\n";
+ 
+# show sheet and cell info
+my $oSheet = $oBook->{Worksheet}[0];
+print "Sheet Name:" , $oSheet->{Name} , "\n";
+print "A1:", $oSheet->{Cells}[0][0]->value, "\n";
