00b6dd46971f3f8dc9223edf34a8591fb8219624
tsuchim <yuji@eikai.co.jp>
2020-02-13 15:59:54 +0900

    xlsxからテンプレートの出力

--- xlsx2html.pl
+++ xlsx2html.pl
@@ -13,6 +13,7 @@ use Spreadsheet::XLSX;
 
 # Open book file
 my $infile = 'data/xlsx/win10.xlsx';
+my $tmpldir = 'data/xlsx';
 my $excel = Spreadsheet::XLSX->new($infile);
  
 # show book info
@@ -38,16 +39,51 @@ foreach my $sheet (@{$excel -> {Worksheet}}) {
 
   # Read Contents from the Sheet
   $row++;
+  my @contents;
   for( ; $row <= $sheet->{MaxRow} ; $row++ ) {
-    $sheet -> {MaxCol} ||= $sheet -> {MinCol};
-    foreach my $col ($sheet -> {MinCol} ..  $sheet -> {MaxCol}) {
-      my $cell = $sheet -> {Cells} [$row] [$col];
+    $sheet->{MaxCol} ||= $sheet->{MinCol};
+    my %rowdata;
+    foreach my $col ($sheet->{MinCol} ..  $sheet->{MaxCol}) {
+      my $cell = $sheet->{Cells}[$row][$col];
       if ($cell) {
         my $str = decode_entities( $cell->{Val});
-        printf("( %s , %s ) => %s\n", $row, $col, $str);
+        # printf("( %s , %s ) => %s\n", $row, $col, $str);
+        my $key = 'class';
+        $key = sprintf('col%u',$col) if( $col );
+        $rowdata{$key} = $str;
       }
     }
+    push( @contents, \%rowdata);
   }
 
-  # Outout HTML using Templete 
+  # Outout HTML using Templete
+  my $tmplname = exists($info{templete}) ? $info{templete} : '';
+  unless( $tmplname ) {
+    $tmplname = $infile;
+    if( $tmplname =~ m!([^/]+)\.xlsx?$! ) {
+      $tmplname = $1;
+    }
+  }
+  print "Open $tmplname.tmpl as HTML Template.\n";
+  my $template = HTML::Template->new(filename=>"$tmpldir/$tmplname.tmpl", die_on_bad_params=>0 );
+  die("Templete cannot open : $tmplname.tmpl") unless $template;
+
+  # set parameters
+  foreach my $key ( keys %info ) {
+    $template->param( $key => $info{$key} );
+  }
+  $template->param( contents0 => shift(@contents) );
+  if( $sheet->{Name} eq 'index' ) {
+    # Set parameters of contents individually for index
+    my $i=0;
+    foreach my $content ( @contents ) {
+      $i++;
+      $template->param( "content$i" => $content );
+    }
+  }else{
+    # Set parameters of contents as array for normal contents
+    $template->param( contents => \@contents );
+  }
+  # Output
+  print $template->output();
 }
